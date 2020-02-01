58a56d0f752d1b716ff1d025e2b2e7b105291006
tenorigatori <60555712+tenorigatori@users.noreply.github.com>
2020-02-01 16:29:48 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,107 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+// The buttons to start & stop stream and to capture the image
+var btnStart = document.getElementById( "btn-start" );
+var btnStop = document.getElementById( "btn-stop" );
+var btnCapture = document.getElementById( "btn-capture" );
+
+// The stream & capture
+var stream = document.getElementById( "stream" );
+var capture = document.getElementById( "capture" );
+var snapshot = document.getElementById( "snapshot" );
+
+// The video stream
+var cameraStream = null;
+
+// Attach listeners
+btnStart.addEventListener( "click", startStreaming );
+btnStop.addEventListener( "click", stopStreaming );
+
+// Start Streaming
+function startStreaming() {
+
+	var mediaSupport = 'mediaDevices' in navigator;
+
+	if( mediaSupport && null == cameraStream ) {
+
+		navigator.mediaDevices.getUserMedia( { video: true } )
+		.then( function( mediaStream ) {
+
+			cameraStream = mediaStream;
+
+			stream.srcObject = mediaStream;
+
+			stream.play();
+		})
+		.catch( function( err ) {
+
+			console.log( "Unable to access camera: " + err );
+		});
+	}
+	else {
+
+		alert( 'Your browser does not support media devices.' );
+
+		return;
+	}
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+
+// Stop Streaming
+function stopStreaming() {
+
+	if( null != cameraStream ) {
+
+		var track = cameraStream.getTracks()[ 0 ];
+
+		track.stop();
+		stream.load();
+
+		cameraStream = null;
+	}
+}
+
+btnCapture.addEventListener( "click", captureSnapshot );
+
+function captureSnapshot() {
+
+	if( null != cameraStream ) {
+
+		var ctx = capture.getContext( '2d' );
+		var img = new Image();
+
+		ctx.drawImage( stream, 0, 0, capture.width, capture.height );
+
+		img.src		= capture.toDataURL( "image/png" );
+		img.width	= 240;
+
+		snapshot.innerHTML = '';
+
+		snapshot.appendChild( img );
+	}
+}
+
+function dataURItoBlob( dataURI ) {
+
+	var byteString = atob( dataURI.split( ',' )[ 1 ] );
+	var mimeString = dataURI.split( ',' )[ 0 ].split( ':' )[ 1 ].split( ';' )[ 0 ];
+	
+	var buffer	= new ArrayBuffer( byteString.length );
+	var data	= new DataView( buffer );
+	
+	for( var i = 0; i < byteString.length; i++ ) {
+	
+		data.setUint8( i, byteString.charCodeAt( i ) );
+	}
+	
+	return new Blob( [ buffer ], { type: mimeString } );
+}
+
+var request = new XMLHttpRequest();
+
+request.open( "POST", "/upload/url", true );
+
+var data	= new FormData();
+var dataURI	= snapshot.firstChild.getAttribute( "src" );
+var imageData   = dataURItoBlob( dataURI );
+
+data.append( "image", imageData, "myimage" );
+
+request.send( data );
